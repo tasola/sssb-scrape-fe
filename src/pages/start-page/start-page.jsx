0676c3e1bf4e79c11e06ad4437b1d2800335d12a
tasola <petter.tk@hotmail.com>
2020-01-07 12:28:12 +0100

    Add basic login form

---
+++ src/pages/start-page/start-page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginPage from '../login-page/login-page'
+
+class StartPage extends React.Component {
+  render() {
+    return <LoginPage />
+  }
+}
+
+export default StartPage

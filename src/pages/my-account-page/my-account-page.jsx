83822eb3bf8d9747348c9a5d1b740869c2684040
tasola <petter.tk@hotmail.com>
2020-07-24 17:03:58 +0200

    Added a skeleton for the delete account component

--- src/pages/my-account-page/my-account-page.jsx
+++ src/pages/my-account-page/my-account-page.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const MyAccountPage = () => {
-  return <div>Hej</div>
+  return (
+    <div>
+      <p>Hej</p>
+      <div>
+        <h3>Delete account</h3>
+        <hr />
+        <p>Lorem ipsum</p>
+        <button>Sad</button>
+        <button>Sad2</button>
+      </div>
+    </div>
+  )
 }
 
 export default MyAccountPage
